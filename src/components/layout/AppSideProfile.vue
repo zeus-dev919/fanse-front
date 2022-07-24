6956d58a3d703a976b23e6024ce56c98c892b6fd
zeus-dev919 <cccodechef@gmail.com>
2022-07-24 21:25:07 +0300

    username class remove

--- src/components/layout/AppSideProfile.vue
+++ src/components/layout/AppSideProfile.vue
@@ -1,28 +1,38 @@
 <template>
   <div>
-    <app-search
-      class="pt-3"
-      v-model="q"
-      @search="search"
-      v-if="$route.name != 'search'"
-    />
-    <app-suggestions />
+    <b-row
+        class="px-3 mt-3 d-block border-bottom pb-3"
+        v-if="!user.isSubscribed && !isOwner"
+      >
+        <h5 class="mb-3">{{ $t("general.subscription") }}</h5>
+        <b-button variant="primary" block @click.prevent="subscribe">
+          {{
+            $t("general.subscribe-for-x", [
+              user.isFree ? $t("general.free") : user.priceFormatted,
+            ])
+          }}
+        </b-button>
+      </b-row>
+      <b-row
+        class="px-3 mt-3 d-block border-bottom pb-3"
+        v-if="
+          !isOwner &&
+          !user.isSubscribed &&
+          !user.isFree &&
+          user.bundles.length > 0
+        "
+      >
+        <h5 class="mb-3">{{ $t("general.subscription-bundles") }}</h5>
+        <b-button
+          variant="primary"
+          block
+          @click.prevent="subscribe(item)"
+          v-for="(item, index) in user.bundles"
+          :key="index"
+        >
+          {{ $t("general.get-x-months-for-y-z-off", item.title(user)) }}
+        </b-button>
+      </b-row>
   </div>
 </template>
-<script>
-import AppSearch from "./AppSearch.vue";
-import AppSuggestions from "./AppSuggestions.vue";
-export default {
-  data: function () {
-    return {
-      q: "",
-    };
-  },
-  components: { AppSuggestions, AppSearch },
-  methods: {
-    search() {
-      this.$router.push("/search?q=" + encodeURIComponent(this.q));
-    },
-  },
-};
-</script>
+
