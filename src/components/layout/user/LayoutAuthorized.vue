a7396c99ac747cecf23d30c3e391a01309fd5a3c
zeus-dev919 <cccodechef@gmail.com>
2022-07-24 17:43:33 +0300

    username class remove

--- src/components/layout/user/LayoutAuthorized.vue
+++ src/components/layout/user/LayoutAuthorized.vue
@@ -131,7 +131,7 @@
           </div>
         </div>
       </b-nav-item>
-      <b-nav-item to="/posts/create" style="width: 80% !important;">
+      <b-nav-item to="/posts/create">
         <div class="icon d-inline-block"><i class="bi-plus-circle" /></div>
       </b-nav-item>
       <b-nav-item to="/messages">
@@ -189,7 +189,6 @@ a.nav-link:hover {
   position: relative;
   font-size: 1.4rem;
   line-height: 1.4rem;
-  color: #8a96a3;
   .bi-dot {
     position: absolute;
     bottom: 0;
