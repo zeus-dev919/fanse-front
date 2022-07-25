97033def1f2afdc08a2342b1a8814091fc7ba77d
zeus-dev919 <cccodechef@gmail.com>
2022-07-25 15:51:42 +0300

    username class remove

--- src/components/ui/UiPost.vue
+++ src/components/ui/UiPost.vue
@@ -149,11 +149,11 @@
             class="position-absolute p-3"
             style="bottom: 0; left: 0; right: 0"
           >
-            <b-button variant="primary" block @click.prevent="subscribe">{{
+            <!-- <b-button variant="primary" block @click.prevent="subscribe">{{
               post.isFree
                 ? $t("general.subscribe-to-see")
                 : $t("general.unlock-post-for-x", [post.priceFormatted])
-            }}</b-button>
+            }}</b-button> -->
           </div> 
            <div v-if="post.user.price>1"
             class="position-absolute p-3"
