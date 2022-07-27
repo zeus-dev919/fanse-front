204ba873e91221b7e7801be94c56cab3d043ead2
zeus-dev919 <cccodechef@gmail.com>
2022-07-27 17:22:08 +0300

    username class remove

--- src/components/layout/user/Layout.vue
+++ src/components/layout/user/Layout.vue
@@ -1,6 +1,6 @@
 <template>
   <layout-authorized v-if="isLoggedIn" />
-  <layout-landing v-else />
+  <layout-authorized v-else />
 </template>
 <script>
 import LayoutAuthorized from "./LayoutAuthorized.vue";
