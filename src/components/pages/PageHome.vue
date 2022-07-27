5ce3d3091f12a03a5c918bfcbc4dd3f4444ef16f
zeus-dev919 <cccodechef@gmail.com>
2022-07-27 19:56:18 +0300

    username class remove

--- src/components/pages/PageHome.vue
+++ src/components/pages/PageHome.vue
@@ -1,4 +1,3 @@
-
 <template>
   <b-row>
     <b-col>
@@ -7,58 +6,10 @@
           <h5 class="text-uppercase my-3">{{ $t("general.home") }}</h5>
         </b-col>
       </b-row>
-      <b-link :to="user.url" class="rounded suggestion w-100 bg-light d-block">
-    <b-img :src="user.cover" v-if="user.cover != null" class="rounded cover" onContextMenu="return false;"  />
-    <b-avatar
-      :src="user.avatar"
-      :text="user.initials"
-      size="100px"
-      class="avatar m-2"
-    />
-    <div class="buttons m-2">
-      <b-dropdown no-caret right variant="link">
-        <template slot="button-content"
-          ><i class="bi-three-dots-vertical text-white"
-        /></template>
-        <b-dropdown-item @click.prevent="copyLink">{{
-          $t("general.copy-link-to-profile")
-        }}</b-dropdown-item>
-        <b-dropdown-item @click.prevent="addToList(user)">{{
-          $t("general.add-to-list")
-        }}</b-dropdown-item>
-      </b-dropdown>
-    </div>
-    <div class="overflow-hidden w-100 subprofile py-2 pr-2">
-      <ui-username :user="user" :asLink="false" class="text-white " style="color:white;"  />
-      
-      <div class="text-white small username-white d-block ">
-        {{ "@" + user.username }}
-      </div>
-    </div>
-      </b-link>
       <ui-posts v-model="posts" />
     </b-col>
   </b-row>
 </template>
-<script>
-import User from "../models/User";
-import UiUsername from "./UiUsername.vue";
-export default {
-  components: { UiUsername },
-  props: {
-    user: User,
-  },
-  methods: {
-    copyLink() {
-      const link = `${process.env.VUE_APP_APP_URL}${this.user.url}`;
-      this.$copyText(link);
-    },
-    addToList(user) {
-      this.$store.state.addToListUser = user;
-    },
-  },
-};
-</script>
 <script>
 import Post from "../models/Post";
 import UiPosts from "../ui/UiPosts.vue";
