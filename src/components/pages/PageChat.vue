e39366f2c28524f8a74196d5fc99c23960e59875
zeus-dev919 <cccodechef@gmail.com>
2022-08-08 21:06:31 +0300

    Sub changes

--- src/components/pages/PageChat.vue
+++ src/components/pages/PageChat.vue
@@ -50,7 +50,7 @@
           </div>
         </div>
       </b-row>
-      <b-row class="border-top" v-if="party.isSubscribed || party.isCreator">
+      <b-row class="border-top" v-if="party.is_subscribed || party.isCreator">
         <ui-media-uploader v-model="media" ref="uploader" />
 
         <b-form-textarea
