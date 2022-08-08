8859edc659b8472ad11be0113362467146d626d2
zeus-dev919 <cccodechef@gmail.com>
2022-08-08 18:50:26 +0300

    Sub changes

--- src/components/ui/UiBuy.vue
+++ src/components/ui/UiBuy.vue
@@ -11,18 +11,18 @@
       body-class="p-0"
     >
       <div v-if="!isTip" class="border-bottom" >
-      <b-link :to="user.url" class="suggestion w-100 bg-light d-block">
-          <b-img :src="user.cover" v-if="user.cover != null" class="cover" onContextMenu="return false;"  />
+      <b-link :to="userbuy.url" class="suggestion w-100 bg-light d-block">
+          <b-img :src="userbuy.cover" v-if="userbuy.cover != null" class="cover" onContextMenu="return false;"  />
           <b-avatar
-            :src="user.avatar"
-            :text="user.initials"
+            :src="userbuy.avatar"
+            :text="userbuy.initials"
             size="100px"
             class="avatar m-2"
           />
           <div class="overflow-hidden w-100 subprofile py-2 pr-2">
-            <ui-username :user="user" :asLink="false" class="text-white " style="color:white;"  />
+            <ui-username :user="userbuy" :asLink="false" class="text-white " style="color:white;"  />
             <div class="text-white small username-white d-block ">
-              {{ "@" + user.username }}
+              {{ "@" + userbuy.username }}
             </div>
           </div>
       </b-link>
@@ -190,7 +190,7 @@ export default {
   components: { UiCreditCard, UiUsername, UiFormInput },
   data: function () {
     return {
-      user: null,
+      userbuy: null,
       gateway: null,
       gateways: [],
       method: null,
@@ -302,8 +302,7 @@ export default {
         this.$get(
           "/users/" + this.$store.state.buyItem.user.username,
           (data) => {
-            this.user = new User(data);
-            this.loadPosts();
+            this.userbuy = new User(data);
           },
           (errors) => {
             console.log(errors);
