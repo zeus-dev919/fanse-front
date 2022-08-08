ff4d0a6bc10ec775a11f74e73d14a6c56215a545
zeus-dev919 <cccodechef@gmail.com>
2022-08-08 13:04:59 +0300

    Ui changes

--- src/components/pages/PageUser.vue
+++ src/components/pages/PageUser.vue
@@ -209,6 +209,7 @@
           v-if="item.months==1">
           {{ $t("general.get-x-months-for-y-z-off", item.title(user),hascamp=true) }}
         </b-button>
+         </p>
          <b-row  class="px-3 mt-3 d-block border-bottom pb-3"
           v-if="!hascamp">
           <h5 class="mb-3">{{ $t("general.subscription") }}</h5>
@@ -220,7 +221,6 @@
             }}
           </b-button>
         </b-row>
-        </p>
       </b-row>
       <b-row
         class="px-3 mt-3 d-block border-bottom pb-3"
