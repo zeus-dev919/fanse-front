3b0c3d8a35eb243ba4f8c31eac8aa6a4873afb2e
zeus-dev919 <cccodechef@gmail.com>
2022-08-10 18:51:05 +0300

    Username change

--- src/components/layout/user/LayoutLanding.vue
+++ src/components/layout/user/LayoutLanding.vue
@@ -3,7 +3,6 @@
     <div class="flex-grow-1 d-flex flex-row">
       <div class="w-50 promo position-relative d-none d-md-block">
         <div class="middle">
-          <ui-logo class="logo mb-3" />
           <h2>{{ $t("general.signup-slogan") }}</h2>
         </div>
       </div>
@@ -61,9 +60,8 @@
 </style>
 <script>
 import AppFooter from "../AppFooter.vue";
-import UiLogo from "../../ui/UiLogo.vue";
 export default {
-  components: { AppFooter, UiLogo },
+  components: { AppFooter },
   computed: {
     appname() {
       return process.env.VUE_APP_APP_NAME;
--- src/components/pages/PageVerification.vue
+++ src/components/pages/PageVerification.vue
@@ -23,11 +23,35 @@
       </b-row>
       <b-row>
         <b-form class="w-100 mx-3 mt-3">
-          <div class="" style="padding-bottom: 30px;">
+          <div class="" style="padding-bottom: 10px;">
             <h4 class="mb-0 font-montserrat"><i class="bi bi-patch-check-fill" style="
                         color: #2081E2;
                     "></i> Become a Verify Creator</h4>
               </div>
+          <div class="btn-block " style="text-align: left !important;">
+            <p class="bollets-home" style="
+                margin-top: 10px;
+                margin-left: 0px;
+                margin-bottom: 10px;
+            "><d class="" style="color: #2081E2;">💕</d> You take home <strong>85%</strong> of your earnings! <strong>Lower than anywhere else!</strong>
+            </p>
+            <p class="bollets-home" style="
+                margin-left: 0px;
+                margin-top: 10px;
+                margin-bottom: 10px;
+            "><d class="" style="color: #2081E2;">💎</d> Legend Creators are getting <strong>social media Promotions FOR FREE!</strong></p>
+            
+          <p class="bollets-home" style="
+              margin-left: 0px;
+              margin-top: 10px;
+              margin-bottom: 10px;
+          "><d class="" style="color: #2081E2;">💸</d> Join the <strong>most converting</strong> social subscription platform!</p>
+          <p class="bollets-home" style="
+                          margin-left: 0px;
+                          margin-top: 10px;
+                          margin-bottom: 10px;
+              "><d class="" style="color: #2081E2;">✔️</d> Fast verification</p>
+        </div>
           <b-alert show variant="secondary" v-if="editing">{{
             $t("general.verification-info")
           }}</b-alert>
@@ -114,9 +138,7 @@
             :errors="errors"
             :label="$t('general.zip')"
           />
-          <b-alert show variant="secondary" v-if="editing">{{
-            $t("general.verification-info-bottom")
-          }}</b-alert>
+          <b-alert show variant="secondary" v-if="editing">Upload a photo with your ID, Make sure the photo you send meets the following requirements:<br />– Both your hand holding the ID card and your full face must be visible  <br />– Your face must be clearly visible in both the photo and the ID <br />– The photo must be well lit and not too small, too dark or too blurry</b-alert>
           <b-form-group
             :label="$t('general.photo-id')"
             label-for="photo"
