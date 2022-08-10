27949a50a4eb418b85b8264f7f3b769a07e4e236
zeus-dev919 <cccodechef@gmail.com>
2022-08-10 17:09:17 +0300

    Username change

--- src/components/layout/AppSide.vue
+++ src/components/layout/AppSide.vue
@@ -7,18 +7,20 @@
       v-if="$route.name != 'search'"
     />
     <app-suggestions />
+     <app-footer />
   </div>
 </template>
 <script>
 import AppSearch from "./AppSearch.vue";
 import AppSuggestions from "./AppSuggestions.vue";
+import AppFooter from "../AppFooter.vue";
 export default {
   data: function () {
     return {
       q: "",
     };
   },
-  components: { AppSuggestions, AppSearch },
+  components: { AppSuggestions, AppSearch,AppFooter },
   methods: {
     search() {
       this.$router.push("/search?q=" + encodeURIComponent(this.q));
