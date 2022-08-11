298e8be88cbb18364cbaaee8b7cd0b462933f332
zeus-dev919 <cccodechef@gmail.com>
2022-08-11 16:16:20 +0300

    add become

--- src/components/pages/PageHome.vue
+++ src/components/pages/PageHome.vue
@@ -6,6 +6,11 @@
           <h5 class="text-uppercase my-3">{{ $t("general.home") }}</h5>
         </b-col>
       </b-row>
+      <div class="box-become">
+        <div class="inner-box"><p class="p-inner">
+          <span class="span-box" style="">Welcome to Fanse 👋</span><br>Start Converting your Fans into Paid followers</p>
+          <div style="flex: 1 1 0%;">
+            <a class="a-box" tabindex="0" href="/payout">Become a creator</a></div></div></div>
       <div v-if="isMobile()" ref="swiper" class="swiper w-100 overflow-hidden" style="margin-top:15px;">
         <div class="swiper-wrapper">
           <div
@@ -26,6 +31,82 @@
     </b-col>
   </b-row>
 </template>
+<style scoped>
+.box-become{
+  background: linear-gradient(95.92deg, rgb(28 115 203) 0%, rgb(29 111 194) 20.05%, rgb(61 133 205) 100%);
+    border-radius: 8px;
+    box-shadow: rgb(0 0 0 / 8%) 0px 4px 10px;
+    margin-bottom: 8px;
+    padding: 16px;
+    margin-top: 10px;
+}
+.inner-box{
+  width: 100%;
+    -webkit-box-align: center;
+    align-items: center;
+    display: inline;
+    flex-direction: row;
+    -webkit-box-pack: justify;
+    justify-content: space-between;
+    text-align: -webkit-center;
+    padding: 10px;
+}
+.p-box{
+  flex: 1 1 0%;
+    margin: 10px;
+    font-family: Roboto, Helvetica, Arial, sans-serif;
+    font-weight: 400;
+    font-size: 1rem;
+    line-height: 1.5;
+    letter-spacing: 0.00938em;
+    color: rgb(255, 255, 255);
+}
+.span-box{
+  margin: 0px;
+    font-family: Roboto, Helvetica, Arial, sans-serif;
+    font-size: 1rem;
+    line-height: 1.5;
+    letter-spacing: 0.00938em;
+    color: inherit;
+    font-weight: bold;
+}
+.a-box{
+    border: none;
+    margin: 8px 0px;
+    display: inline-flex;
+    -webkit-box-align: center;
+    align-items: center;
+    -webkit-box-pack: center;
+    justify-content: center;
+    position: relative;
+    box-sizing: border-box;
+    -webkit-tap-highlight-color: transparent;
+    outline: 0px;
+    border: 0px;
+    margin: 0px;
+    cursor: pointer;
+    user-select: none;
+    vertical-align: middle;
+    appearance: none;
+    text-decoration: none;
+    font-family: Roboto, Helvetica, Arial, sans-serif;
+    line-height: 1.75;
+    min-width: 64px;
+    width: 100%;
+    box-shadow: none;
+    border-radius: 25px;
+    text-transform: initial;
+    white-space: nowrap;
+    letter-spacing: 0.025em;
+    font-weight: 700;
+    transition: color 0.01s ease 0s;
+    height: 48px;
+    font-size: 1rem;
+    padding: 6px 24px;
+    background: #fff;
+    color: #2196f3;
+}
+</style>
 <script>
 import { Swiper } from "swiper";
 import "swiper/swiper-bundle.css";
