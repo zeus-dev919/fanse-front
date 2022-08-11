29b187e3df8e4c2cf649206c97a682747d188223
zeus-dev919 <cccodechef@gmail.com>
2022-08-11 17:53:52 +0300

    add become

--- src/components/ui/UiPost.vue
+++ src/components/ui/UiPost.vue
@@ -151,13 +151,13 @@
             class="position-absolute p-3"
             style="bottom: 0; left: 0; right: 0"
           >
-            <!-- <b-button variant="primary" block @click.prevent="subscribe">{{
+            <b-button variant="primary" block @click.prevent="subscribe">{{
               post.isFree
                 ? $t("general.subscribe-to-see")
                 : $t("general.unlock-post-for-x", [post.priceFormatted])
-            }}</b-button> -->
+            }}</b-button>
           </div> 
-           <div v-if="post.user.price>1"
+           <div v-if="post.user.price>1 && post.user.isSubscribed"
             class="position-absolute p-3"
             style="bottom: 0; left: 0; right: 0"
           >
@@ -166,6 +166,20 @@
                 ? $t("general.subscribe-to-see")
                 : $t("general.unlock-post-for-x", [post.priceFormatted])
             }}</b-button>
+          </div> 
+          <div v-if="post.price>1 && post.user.isSubscribed"
+            class="position-absolute p-3"
+            style="bottom: 0; left: 0; right: 0"
+          >
+            <b-button variant="primary" block @click.prevent="unlock">{{
+              $t("general.unlock-post-for-x", [post.priceFormatted])
+            }}</b-button>
+          </div> 
+           <div v-if="post.user.price>1 && !post.user.isSubscribed"
+            class="position-absolute p-3"
+            style="bottom: 0; left: 0; right: 0"
+          >
+            <b-button variant="primary" block @click.prevent="subscribe">{{$t("general.subscribe-to-see")}}</b-button>
           </div> 
           
         </div>
