c93d82b78f2baafb426c0b990045c7bf645b6132
zeus-dev919 <cccodechef@gmail.com>
2022-08-17 16:53:15 +0300

    back

--- src/components/layout/user/LayoutLanding.vue
+++ src/components/layout/user/LayoutLanding.vue
@@ -80,7 +80,7 @@
     box-sizing: inherit;
     margin: 0;
     font-family: "system-ui";
-    font-size: 1.125rem;
+    font-size: 2.125rem;
     letter-spacing: 0.00735em;
     text-align: left;
     color: 000;
@@ -107,7 +107,7 @@
     color: white;
     font-family: "system-ui";
     font-weight: bold;
-    font-size: 2rem;
+    font-size: 5rem;
     line-height: 1.167;
     text-align: left;
     box-sizing: inherit;
@@ -124,7 +124,7 @@
     color: white;
     font-family: "system-ui";
     font-weight: bold;
-    font-size: 3rem;
+    font-size: 5rem;
     line-height: 1.167;
     letter-spacing: -0.01562em;
     text-align: left;
@@ -194,8 +194,8 @@
     box-sizing: border-box;
     margin-right: auto;
     display: block;
-    padding-left: 24px;
-    padding-right: 24px;
+    padding-left: 14px;
+    padding-right: 14px;
     max-width: 1150px;
 }
 .home-slide-2{
@@ -253,6 +253,16 @@
 {
   display: block !important;
 }
+.home-h3{
+font-size: 1.125rem  !important;
+}
+.home-h2{
+  font-size: 2rem !important;
+}
+.home-dot{
+  font-size: 3rem  !important;
+}
+
 }
 @media only screen and (min-width: 800px) {
  .main-login
