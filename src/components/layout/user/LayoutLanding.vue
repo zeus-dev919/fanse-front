4308515c412966cb18c4a089d34bab4d5e9aca07
zeus-dev919 <cccodechef@gmail.com>
2022-08-17 19:50:22 +0300

    back

--- src/components/layout/user/LayoutLanding.vue
+++ src/components/layout/user/LayoutLanding.vue
@@ -42,6 +42,164 @@
             <a class="a-box" tabindex="0" href="/payouts">Become a creator</a></div>
         </div>
       </div>
+      <section class="faq">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-xl-8 col-lg-8 text-center">
+                    <div class="section-title">
+                        <h4>FAQ's</h4>
+                        <h2>Frequently Asking <span>Question’s</span></h2>
+                    </div>
+                </div>
+            </div>
+    
+            <div class="row">
+                <div class="col-xl-12 col-lg-12">
+                    <div class="accordion" id="accordionExample">
+                        <div class="row">
+                            <div class="col-xl-6 col-lg-6">
+                                <div class="card">
+                                    <div class="card-header">
+                                        <h5 class="mb-0">
+                                            <button class="btn btn-link collapsed" type="button" data-toggle="collapse"
+                                                data-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+                                                What is Lorem Ipsum?
+                                            </button>
+                                        </h5>
+                                    </div>
+    
+                                    <div id="collapseOne" class="collapse" data-parent="#accordionExample">
+                                        <div class="card-body">
+                                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer.
+                                        </div>
+                                    </div>
+                                </div>
+    
+                                <div class="card">
+                                    <div class="card-header">
+                                        <h5 class="mb-0">
+                                            <button class="btn btn-link collapsed" type="button" data-toggle="collapse"
+                                                data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                                                Where does it come from?
+                                            </button>
+                                        </h5>
+                                    </div>
+                                    <div id="collapseTwo" class="collapse" data-parent="#accordionExample">
+                                        <div class="card-body">
+                                            Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. 
+                                        </div>
+                                    </div>
+                                </div>
+    
+                                <div class="card">
+                                    <div class="card-header">
+                                        <h5 class="mb-0">
+                                            <button class="btn btn-link collapsed" type="button" data-toggle="collapse"
+                                                data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                                               Why do we use it?
+                                            </button>
+                                        </h5>
+                                    </div>
+                                    <div id="collapseThree" class="collapse" data-parent="#accordionExample">
+                                        <div class="card-body">
+                                            It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. 
+                                        </div>
+                                    </div>
+                                </div>
+    
+                                <div class="card">
+                                    <div class="card-header">
+                                        <h5 class="mb-0">
+                                            <button class="btn btn-link collapsed" type="button" data-toggle="collapse"
+                                                data-target="#collapse4" aria-expanded="false" aria-controls="collapseThree">
+                                                Where can I get some?
+                                            </button>
+                                        </h5>
+                                    </div>
+                                    <div id="collapse4" class="collapse" data-parent="#accordionExample">
+                                        <div class="card-body">
+                                            There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+    
+                            <div class="col-xl-6 col-lg-6">
+                                <div class="card">
+                                    <div class="card-header">
+                                        <h5 class="mb-0">
+                                            <button class="btn btn-link collapsed" type="button" data-toggle="collapse"
+                                                data-target="#collapse5" aria-expanded="false" aria-controls="collapseOne">
+                                                What is Lorem Ipsum?
+                                            </button>
+                                        </h5>
+                                    </div>
+    
+                                    <div id="collapse5" class="collapse" data-parent="#accordionExample">
+                                        <div class="card-body">
+                                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer.
+                                        </div>
+                                    </div>
+                                </div>
+    
+                                <div class="card">
+                                    <div class="card-header">
+                                        <h5 class="mb-0">
+                                            <button class="btn btn-link collapsed" type="button" data-toggle="collapse"
+                                                data-target="#collapse6" aria-expanded="false" aria-controls="collapseTwo">
+                                                Where does it come from?
+                                            </button>
+                                        </h5>
+                                    </div>
+                                    <div id="collapse6" class="collapse" data-parent="#accordionExample">
+                                        <div class="card-body">
+                                            Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. 
+                                        </div>
+                                    </div>
+                                </div>
+    
+                                <div class="card">
+                                    <div class="card-header">
+                                        <h5 class="mb-0">
+                                            <button class="btn btn-link collapsed" type="button" data-toggle="collapse"
+                                                data-target="#collapse7" aria-expanded="false" aria-controls="collapseThree">
+                                                Why do we use it?
+                                            </button>
+                                        </h5>
+                                    </div>
+                                    <div id="collapse7" class="collapse" data-parent="#accordionExample">
+                                        <div class="card-body">
+                                            It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="card">
+                                    <div class="card-header">
+                                        <h5 class="mb-0">
+                                            <button class="btn btn-link collapsed" type="button" data-toggle="collapse"
+                                                data-target="#collapse8" aria-expanded="false" aria-controls="collapseThree">
+                                                Where can I get some?
+                                            </button>
+                                        </h5>
+                                    </div>
+                                    <div id="collapse8" class="collapse" data-parent="#accordionExample">
+                                        <div class="card-body">
+                                            
+                                            There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour
+                                        </div>
+                                    </div>
+                                </div>
+    
+                            </div>
+    
+                        </div>
+    
+                    </div>
+    
+                </div>
+            </div>
+        </div>
+    </section>
       </div>
     </div>
     </div>
@@ -50,6 +208,143 @@
 </template>
 <style scoped lang="scss">
 @import "~@/assets/scss/_variables.scss";
+h1 {
+	color: #ff008b;
+}
+h2 {
+	color: #ff008b;
+}
+h3 {
+	color: #ff008b;
+}
+h4 {
+	color: #ff008b;
+}
+h5 {
+	color: #ff008b;
+}
+h6 {
+	color: #ff008b;
+}
+section {
+	padding: 60px;
+}
+a {
+	text-decoration: none;
+	outline: none;
+	&:hover {
+		text-decoration: none;
+		outline: none;
+	}
+	&:focus {
+		text-decoration: none;
+		outline: none;
+	}
+	&:active {
+		text-decoration: none;
+		outline: none;
+	}
+}
+ul {
+	margin: 0;
+	padding: 0;
+	list-style: none;
+}
+.faq {
+	padding: 120px 0 90px;
+	background: #f7fbff;
+	min-height: 100vh;
+	.section-title {
+		margin-bottom: 54px;
+		h2 {
+			margin-bottom: 22px;
+		}
+	}
+	.accordion {
+		.card {
+			border: none;
+			margin-bottom: 30px;
+			&:not(:first-of-type) {
+				.card-header {
+					&:first-child {
+						border-radius: 10px;
+					}
+				}
+			}
+			.card-header {
+				border: none;
+				border-radius: 10px;
+				padding: 0;
+				h5 {
+					padding: 0;
+					button {
+						color: #1e3056;
+						font-size: 18px;
+						font-weight: 500;
+						text-decoration: none;
+						padding: 0 30px 0 70px;
+						height: 80px;
+						display: block;
+						width: 100%;
+						color: rgba(30, 48, 86, 0.8);
+						text-align: left;
+						background: #fff;
+						-webkit-box-shadow: 0px -50px 140px 0px rgba(69, 81, 100, 0.1);
+						box-shadow: 0px -50px 140px 0px rgba(69, 81, 100, 0.1);
+						border-radius: 10px 10px 0 0;
+						position: relative;
+						&:after {
+							position: absolute;
+							left: 30px;
+							top: 50%;
+							margin-top: -10px;
+							width: 20px;
+							height: 20px;
+							background-color: transparent;
+							color: #ff5f74;
+							text-align: center;
+							border: 1px solid #ff5f74;
+							border-radius: 50%;
+							line-height: 100%;
+							content: '\f067';
+							font-size: 10px;
+							line-height: 18px;
+							font-family: "Font Awesome 5 Free";
+							font-weight: 900;
+						}
+					}
+					button.collapsed {
+						background: #fff;
+						border-radius: 10px;
+						-webkit-box-shadow: none;
+						box-shadow: none;
+						border: 1px solid rgba(97, 125, 255, 0.2);
+					}
+					button[aria-expanded="true"] {
+						&:after {
+							content: '\f068';
+							color: #fff;
+							background-image: -webkit-linear-gradient(-180deg, #5e7eff 0%, #ff5f74 30%, #a85fff 100%);
+						}
+					}
+				}
+			}
+			.card-body {
+				-webkit-box-shadow: 0px 15px 140px 0px rgba(69, 81, 100, 0.1);
+				box-shadow: 0px 15px 140px 0px rgba(69, 81, 100, 0.1);
+				border-radius: 0 0 10px 10px;
+				padding-top: 0;
+				margin-top: -6px;
+				padding-left: 72px;
+				padding-right: 70px;
+				padding-bottom: 23px;
+				color: rgba(30, 48, 86, 0.8);
+				line-height: 30px;
+			}
+		}
+	}
+}
+
 .promo {
   background: $primary;
   color: $white;
