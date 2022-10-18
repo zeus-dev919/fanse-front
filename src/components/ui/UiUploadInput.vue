6f782814cb4afa823d030fdfeea4680bef2da3d9
zeus-dev919 <cccodechef@gmail.com>
2022-10-18 06:41:28 +0600

    fixes

--- src/components/ui/UiUploadInput.vue
+++ src/components/ui/UiUploadInput.vue
@@ -1,20 +1,23 @@
 <template>
-    <!-- Styled -->
-    <b-form-file
-      v-model="file1"
-      :state="Boolean(file1)"
-      placeholder="Choose a file or drop it here..."
-      drop-placeholder="Drop file here..."
-    ></b-form-file>
+  <div class="space-y-1 p-2 text-center">
+    <div class="flex text-sm text-gray-600 text-center">
+      <label
+        for="file-upload"
+        class="relative cursor-pointer bg-transparent rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500"
+    
+        ><span><i class="bi-image" /> Upload a file</span
+        ><input id="file-upload" name="file-upload" type="file" class="sr-only"
+      /></label>
+    </div>
+  </div>
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        file1: null,
-        file2: null
-      }
+export default {
+  data() {
+    return {
+      
     }
-  }
+  },
+}
 </script>
