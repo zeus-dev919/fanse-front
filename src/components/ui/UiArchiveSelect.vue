7de4398ef96b5bb551a455e71e664ca0ae46aaab
zeus-dev919 <cccodechef@gmail.com>
2022-10-19 23:31:16 -0700

    Add Archive to Post&Message Page.
    create archiveSelect modal & implement to post & message page.

---
+++ src/components/ui/UiArchiveSelect.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="archives">
+      <div v-for="(archive, key) in archives" :key="key">
+          <div v-if="archive != null" @click.prevent="addArchive(archive)">
+            <b-img v-if="archive.type == 0"
+              width="170"
+              height="100"
+              thumbnail fluid
+              :src="archive.url"
+            >
+            </b-img>
+          </div>
+          <div v-if="archive != null" @click.prevent="addArchive(archive)">
+            <video v-if="archive.type == 1"
+            :poster="archive.screenshot"
+            width="170"
+            height="100"
+            controls
+            >
+              <source
+                :src="archive.url"
+                type="video/mp4"
+              />
+            </video>
+        </div>
+      </div>
+    </div>
+  </template>
+  <script>
+  export default {
+    props: ["archives"],
+    components: {},
+  };
+  </script>
