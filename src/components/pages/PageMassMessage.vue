92497124963af88019375d504994cee99dbe610b
zeus-dev919 <cccodechef@gmail.com>
2022-10-31 10:58:08 -0700

    Add archive to MassMessage
    Add archive button to mass message page.

--- src/components/pages/PageMassMessage.vue
+++ src/components/pages/PageMassMessage.vue
@@ -91,7 +91,7 @@
       </b-row>
       <b-row class="border-top">
         <ui-media-uploader v-model="media" ref="uploader" />
-
+        <ui-archive v-if="selectedArchives.length > 0" :archives="selectedArchives"></ui-archive>
         <b-form-textarea
           max-rows="4"
           class="px-3 py-0 my-3"
@@ -112,6 +112,9 @@
         <div class="d-flex align-items-center w-100 mb-2">
           <b-link @click="mediaDropzoneClick" class="mx-2 ml-3">
             <i class="bi-image" />
+          </b-link>
+          <b-link v-if="currentUser.isCreator" v-b-modal.modalArchive class="mx-2">
+            <i class="bi-folder" />
           </b-link>
           <b-link v-b-modal.modalPrice class="mx-2">
             <i class="bi-tag" />
@@ -145,10 +148,61 @@
             />
           </b-input-group>
         </b-modal>
+        <b-modal
+          id="modalArchive"
+          centered
+          ok-only
+          @ok="addSelectArchives()"
+          @cancel="deleteSelectArchives()"
+          @hide="deleteSelectArchives()"
+          :title="$t('general.archive')"
+        >
+          <div>
+            <b-button class="mr-2" style="margin-bottom:15px" variant="outline-primary" @click.prevent="buildArchives('all')">All</b-button>
+            <b-button class="mr-2" style="margin-bottom:15px" variant="outline-primary" @click.prevent="buildArchives('photos')">Photos</b-button>
+            <b-button class="mr-2" style="margin-bottom:15px" variant="outline-primary" @click.prevent="buildArchives('videos')">Videos</b-button>
+
+            <div class="archives">
+              <b-row v-for="(row, key) in archive_rows" :key="key">
+                <b-col v-for="(archive, k) in row" :key="k" style="background-color: #d2d7db61;padding: 10px;text-align: center;border-radius: 10px;margin: 10px;" :class="isExist(archive) ? 'item-border' : ''" @click.prevent="addArchive(archive)"> 
+                  <div  v-if="archive.type == 0">
+                    <b-img
+                      width="100%"
+                      height="100%"
+                      thumbnail fluid
+                      :src="archive.url"
+                    >
+                    </b-img>
+                  </div>
+                  <div v-if="archive.type == 1" >
+                    <video 
+                    :poster="archive.screenshot"
+                    width="100%"
+                    height="100%"
+                    controls
+                    >
+                    </video>
+                </div>
+                </b-col>
+              </b-row>
+            </div>
+          </div>
+        </b-modal>
       </b-row>
     </b-col>
   </b-row>
 </template>
+<style lang="scss" scoped>
+.archives {
+  overflow-y: scroll;
+  overflow-x: hidden;
+  max-height: 400px;
+  padding: 10px;
+}
+.item-border {
+  border:#2081E2 2px solid;
+}
+</style>
 <style scoped lang="scss">
 @import "~@/assets/scss/_variables.scss";
 textarea.form-control {
@@ -190,6 +244,9 @@ textarea.form-control:focus {
 <script>
 import List from "../models/List";
 import UiMediaUploader from "../ui/UiMediaUploader.vue";
+import UiArchive from "../ui/UiArchiveSelect.vue";
+import Media from "../models/Media";
+
 import UiPostOptionInfo from "../ui/UiPostOptionInfo.vue";
 import Swiper from "swiper";
 import "swiper/swiper-bundle.css";
@@ -205,6 +262,12 @@ export default {
       lists: [],
       include: [],
       exclude: [],
+      archives: [],
+      archive_rows: [],
+      archivepage: 1,
+      archivehasMore: true,
+      selectArchives: [],
+      selectedArchives: [],
     };
   },
   computed: {
@@ -219,16 +282,24 @@ export default {
         this.$emit("input", val);
       },
     },
+    currentUser() {
+      return this.$store.state.currentUser;
+    },
   },
   components: {
     UiMediaUploader,
     UiPostOptionInfo,
+    UiArchive
   },
   mounted() {
     this.loadLists();
+    this.loadArchives();
+    window.addEventListener("scroll", this.updateArchiveScroll);
   },
   methods: {
     loadLists() {
+      this.media = [];
+      this.archives = [];
       this.$get(
         "/lists/message",
         (data) => {
@@ -287,7 +358,12 @@ export default {
           screenshot: m.scr ? m.scr.id : null,
         });
       }
-
+      for (let archive of this.selectedArchives) {
+        media.push({
+          id: archive.id,
+          screenshot: archive.scr ? archive.scr.id : null,
+        });
+      }
       this.errors = {};
       let fields = {
         message: this.message,
@@ -317,6 +393,88 @@ export default {
           this.errors = errors;
         }
       );
+    },
+    updateArchiveScroll() {
+      const scrollPosition = window.innerHeight + window.scrollY;
+      if (
+        document.body.offsetHeight &&
+        scrollPosition &&
+        document.body.offsetHeight - scrollPosition <= 1000 &&
+        !this.isLoading &&
+        this.archivehasMore
+      ) {
+        this.loadArchiveMore();
+      }
+    },
+    loadArchives() {
+      this.$get(
+        "/media?page=" + this.archivepage,
+        (data) => {
+          let archives = [...this.archives];
+          for (let obj of data.media.data) {
+            let archive = new Media(obj)
+            if(archive.type !== Media.TYPE_AUDIO && !archive.url.includes("/tmp/"))
+            {
+              archives.push(new Media(obj));
+            }
+          }
+          this.archives = archives;
+          this.archivehasMore = data.media.next_page_url != null;
+          this.buildArchives("all");
+        },
+        (errors) => {
+          console.log(errors);
+        }
+      );
+    },
+    buildArchives(media_type) {
+      this.archive_rows = [];
+      let current_row = [];
+      let index = 0;
+      for (let archive of this.archives)
+      {
+        if(media_type == "videos" && archive.type !== Media.TYPE_VIDEO) continue;
+        if(media_type == "photos" && archive.type !== Media.TYPE_IMAGE) continue;
+
+        if(index % 3 == 0 && index != 0)
+        {
+          this.archive_rows.push([...current_row]);
+          current_row = [];
+        }
+        current_row.push(archive);
+        index++;
+      }
+    },
+    loadArchiveMore() {
+      if (this.archivehasMore) {
+        this.archivepage = this.archivepage + 1;
+        this.loadArchives();
+      }
+    },
+    isExist(item) {
+      if (this.selectArchives.indexOf(item) < 0) return false;
+      return true;
+    },
+    addArchive(item){
+      if (this.selectArchives.indexOf(item) < 0)
+      {
+        this.selectArchives.push(item);
+      }
+      else {
+        this.selectArchives.splice(this.selectArchives.indexOf(item),1);
+      }
+    },
+    addSelectArchives(){
+      if (this.selectArchives != null){
+        for (let archive of this.selectArchives){
+          this.selectedArchives.push(archive);
+        }
+      }
+      console.log(this.selectArchives);
+      this.selectArchives = [];
+    },
+    deleteSelectArchives(){
+      this.selectArchives = [];
     },
   },
 };
